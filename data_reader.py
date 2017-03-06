1a387b41dd069f235eff13ffd6c3239cbd745042
Hongyang Gao <hongyang.gao@wsu.edu>
2017-03-06 15:28:59 -0800

    pep8 format

--- data_reader.py
+++ data_reader.py
@@ -9,35 +9,43 @@ class BatchDataReader(object):
         images, labels = self.read_data(data_dir, data_list)
         images = tf.convert_to_tensor(images, dtype=tf.string)
         labels = tf.convert_to_tensor(labels, dtype=tf.string)
-        queue = tf.train.slice_input_producer([images, labels], name=self.scope+'/slice')
+        queue = tf.train.slice_input_producer(
+            [images, labels], name=self.scope+'/slice')
         self.image, self.label = self.read_dataset(queue, input_size)
 
     def next_batch(self, batchsize):
-        image_batch, label_batch = tf.train.batch([self.image, self.label], batchsize, name=self.scope+'/batch')
-        label_batch = tf.one_hot(tf.squeeze(label_batch, axis=[3], name=self.scope+'/squeeze'),
-            depth=21, name=self.scope+'/one_hot')
+        image_batch, label_batch = tf.train.batch(
+            [self.image, self.label], batchsize, name=self.scope+'/batch')
+        label_batch = tf.squeeze(
+            label_batch, axis=[3], name=self.scope+'/squeeze')
+        label_batch = tf.one_hot(
+            label_batch, depth=21, name=self.scope+'/one_hot')
         return image_batch, label_batch
 
     def read_dataset(self, queue, input_size):
-        image = tf.image.decode_jpeg(tf.read_file(queue[0]), channels=3, name=self.scope+'/image')
-        label = tf.image.decode_png(tf.read_file(queue[1]), channels= 1, name=self.scope+'/label')
+        image = tf.image.decode_jpeg(
+            tf.read_file(queue[0]), channels=3, name=self.scope+'/image')
+        label = tf.image.decode_png(
+            tf.read_file(queue[1]), channels=1, name=self.scope+'/label')
         image = tf.image.resize_images(image, input_size)
         label = tf.image.resize_images(label, input_size, 1)
-        image -= tf.reduce_mean(tf.cast(image, dtype= tf.float32), (0, 1), name=self.scope+'/mean')
+        image -= tf.reduce_mean(tf.cast(image, dtype=tf.float32),
+                                (0, 1), name=self.scope+'/mean')
         return image, label
 
     def read_data(self, data_dir, data_list):
         with open(data_list, 'r') as f:
             images, labels = [], []
             for line in f:
-                image, label= line.strip('\n').split(' ')
+                image, label = line.strip('\n').split(' ')
                 images.append(data_dir + image)
                 labels.append(data_dir + label)
         return images, labels
 
     def start(self):
         self.coord = tf.train.Coordinator()
-        self.threads = tf.train.start_queue_runners(coord=self.coord, sess=self.sess)
+        self.threads = tf.train.start_queue_runners(
+            coord=self.coord, sess=self.sess)
 
     def close(self):
         self.coord.request_stop()
--- network.py
+++ network.py
@@ -8,21 +8,28 @@ class DilatedPixelCNN(object):
     def __init__(self, sess, conf):
         self.sess = sess
         self.conf = conf
-        self.conv_kernel_size = (3, 3)
-        self.pool_kernel_size = (2, 2)
+        self.conv_size = (3, 3)
+        self.pool_size = (2, 2)
         if conf.use_gpu:
             self.data_format = 'NCHW'
             self.axis, self.channel_axis = (2, 3), 1
-            self.input_shape = [self.conf.batch, self.conf.channel, self.conf.height, self.conf.width]
-            self.output_shape = [self.conf.batch, self.conf.class_num, self.conf.height, self.conf.width]
+            self.input_shape = [
+                conf.batch, conf.channel, conf.height, conf.width]
+            self.output_shape = [
+                conf.batch, conf.class_num, conf.height, conf.width]
         else:
             self.data_format = 'NHWC'
             self.axis, self.channel_axis = (1, 2), 3
-            self.input_shape = [self.conf.batch, self.conf.height, self.conf.width, self.conf.channel]
-            self.output_shape = [self.conf.batch, self.conf.height, self.conf.width, self.conf.class_num]
-        input_params = (self.sess, self.conf.data_dir, self.conf.data_list, (self.conf.height, self.conf.width))
+            self.input_shape = [
+                conf.batch, conf.height, conf.width, conf.channel]
+            self.output_shape = [
+                conf.batch, conf.height, conf.width, conf.class_num]
+        input_params = (
+            self.sess, conf.data_dir, conf.data_list,
+            (conf.height, conf.width))
         self.data_reader = BatchDataReader(*input_params)
-        self.inputs, self.annotations = self.data_reader.next_batch(self.conf.batch)
+        self.inputs, self.annotations = self.data_reader.next_batch(
+            self.conf.batch)
         self.build_network()
         tf.set_random_seed(conf.random_seed)
         self.sess.run(tf.global_variables_initializer())
@@ -33,40 +40,59 @@ class DilatedPixelCNN(object):
         for layer_index in range(self.conf.network_depth-1):
             is_first = True if not layer_index else False
             name = 'down%s' % layer_index
-            outputs = self.construct_down_block(outputs, name, down_outputs, first=is_first)
+            outputs = self.construct_down_block(
+                outputs, name, down_outputs, first=is_first)
         outputs = self.construct_bottom_block(outputs, 'bottom')
         for layer_index in range(self.conf.network_depth-2, -1, -1):
-            is_final = True if layer_index==0 else False
+            is_final = True if layer_index == 0 else False
             name = 'up%s' % layer_index
             down_inputs = down_outputs[layer_index]
-            outputs = self.construct_up_block(outputs, down_inputs, name, final=is_final)
+            outputs = self.construct_up_block(
+                outputs, down_inputs, name, final=is_final)
         self.prediction = outputs
         self.loss_op = tf.reduce_mean(
             tf.losses.softmax_cross_entropy(self.annotations, self.prediction))
-        self.train_op = tf.train.AdamOptimizer(self.conf.learning_rate).minimize(self.loss_op, name='train_op')
+        self.train_op = tf.train.AdamOptimizer(
+            self.conf.learning_rate).minimize(self.loss_op, name='train_op')
 
     def construct_down_block(self, inputs, name, down_outputs, first=False):
-        num_outputs = self.conf.start_channel_num if first else 2*inputs.shape[self.channel_axis].value
-        conv1 = conv2d(inputs, num_outputs, self.conv_kernel_size, name+'/conv1', self.data_format)
-        conv2 = conv2d(conv1, num_outputs, self.conv_kernel_size, name+'/conv2', self.data_format)
+        num_outputs = self.conf.start_channel_num if first else 2 * \
+            inputs.shape[self.channel_axis].value
+        conv1 = conv2d(
+            inputs, num_outputs, self.conv_size, name+'/conv1',
+            self.data_format)
+        conv2 = conv2d(
+            conv1, num_outputs, self.conv_size, name+'/conv2',
+            self.data_format)
         down_outputs.append(conv2)
-        pool = pool2d(conv2, self.pool_kernel_size, name+'/pool', self.data_format)
+        pool = pool2d(
+            conv2, self.pool_size, name+'/pool', self.data_format)
         return pool
 
     def construct_bottom_block(self, inputs, name):
         num_outputs = inputs.shape[self.channel_axis].value
-        conv1 = conv2d(inputs, 2*num_outputs, self.conv_kernel_size, name+'/conv1', self.data_format)
-        conv2 = conv2d(conv1, num_outputs, self.conv_kernel_size, name+'/conv2', self.data_format)
+        conv1 = conv2d(
+            inputs, 2*num_outputs, self.conv_size, name+'/conv1',
+            self.data_format)
+        conv2 = conv2d(
+            conv1, num_outputs, self.conv_size, name+'/conv2',
+            self.data_format)
         return conv2
 
     def construct_up_block(self, inputs, down_inputs, name, final=False):
         num_outputs = inputs.shape[self.channel_axis].value
-        conv1 = dilated_conv(inputs, num_outputs, self.conv_kernel_size, name+'/conv1',
+        conv1 = dilated_conv(
+            inputs, num_outputs, self.conv_size, name+'/conv1',
             self.axis, self.data_format)
-        conv1 = tf.concat([conv1, down_inputs], self.channel_axis, name=name+'/concat')
-        conv2 = conv2d(conv1, num_outputs, self.conv_kernel_size, name+'/conv2', self.data_format)
+        conv1 = tf.concat(
+            [conv1, down_inputs], self.channel_axis, name=name+'/concat')
+        conv2 = conv2d(
+            conv1, num_outputs, self.conv_size, name+'/conv2',
+            self.data_format)
         num_outputs = self.conf.class_num if final else num_outputs/2
-        conv3 = conv2d(conv2, num_outputs, self.conv_kernel_size, name+'/conv3', self.data_format)
+        conv3 = conv2d(
+            conv2, num_outputs, self.conv_size, name+'/conv3',
+            self.data_format)
         return conv3
 
     def train(self):
