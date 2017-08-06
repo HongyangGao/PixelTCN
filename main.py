6fd0aa47a3e1c60ef10843021a4164f3af8ea7b9
Hongyang Gao <hongyang.gao@wsu.edu>
2017-08-06 11:35:38 -0700

    change to 2d default

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ This file provides configuration to build U-NET for semantic segmentation.
 def configure():
     # training
     flags = tf.app.flags
-    flags.DEFINE_integer('max_step', 10000, '# of step for training')
+    flags.DEFINE_integer('max_step', 10, '# of step for training')
     flags.DEFINE_integer('test_interval', 100, '# of interval to test a model')
-    flags.DEFINE_integer('save_interval', 1000, '# of interval to save a model')
-    flags.DEFINE_integer('summary_interval', 100, '# of step to save the summary')
+    flags.DEFINE_integer('save_interval', 1000, '# of interval to save  model')
+    flags.DEFINE_integer('summary_interval', 100, '# of step to save summary')
     flags.DEFINE_float('learning_rate', 1e-3, 'learning rate')
     # data
     flags.DEFINE_string('data_dir', './dataset/', 'Name of data directory')
-    flags.DEFINE_string('train_data', 'training3d.h5', 'Training data')
-    flags.DEFINE_string('valid_data', 'validation3d.h5', 'Validation data')
-    flags.DEFINE_string('test_data', 'testing3d.h5', 'Testing data')
-    flags.DEFINE_string('data_type', '3D', '2D data or 3D data')
+    flags.DEFINE_string('train_data', 'training.h5', 'Training data')
+    flags.DEFINE_string('valid_data', 'validation.h5', 'Validation data')
+    flags.DEFINE_string('test_data', 'testing.h5', 'Testing data')
+    flags.DEFINE_string('data_type', '2D', '2D data or 3D data')
     flags.DEFINE_integer('batch', 2, 'batch size')
-    flags.DEFINE_integer('channel', 1, 'channel size')
+    flags.DEFINE_integer('channel', 3, 'channel size')
     flags.DEFINE_integer('depth', 16, 'depth size')
-    flags.DEFINE_integer('height', 128, 'height size')
-    flags.DEFINE_integer('width', 128, 'width size')
+    flags.DEFINE_integer('height', 256, 'height size')
+    flags.DEFINE_integer('width', 256, 'width size')
     # Debug
     flags.DEFINE_string('logdir', './logdir', 'Log dir')
     flags.DEFINE_string('modeldir', './modeldir', 'Model dir')
@@ -39,8 +39,8 @@ def configure():
     flags.DEFINE_integer('random_seed', int(time.time()), 'random seed')
     # network architecture
     flags.DEFINE_integer('network_depth', 5, 'network depth for U-Net')
-    flags.DEFINE_integer('class_num', 4, 'output class number')
-    flags.DEFINE_integer('start_channel_num', 16,
+    flags.DEFINE_integer('class_num', 21, 'output class number')
+    flags.DEFINE_integer('start_channel_num', 64,
                          'start number of outputs for the first conv layer')
     flags.DEFINE_string(
         'conv_name', 'conv',
